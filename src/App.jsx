7b25c7e0c4a0f8e1fd4ef66fc088f34dce05b67b
Soren <S_Gravesen92@hotmail.com>
2019-02-23 13:52:37 +0100

    Setup input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import styled from 'styled-components';
+import SearchInput from './js/components/SearchInput.jsx';
+
+const Wrapper = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  background: #bedad9;
+`;
+
+const Page = styled.div`
+  box-sizing: border-box;
+  padding: 30px 50px;
+  width: 100%;
+  height: 100%;
+  max-width: 1400px;
+  margin: 0 auto;
+`;
+
+const Heading = styled.h1`
+  text-align: center;
+  font-weight: bold;
+  font-style: italic;
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bookResults: null,
+    }
+
+    this.searchBooks = this.searchBooks.bind(this);
+  }
+
+  searchBooks(query) {
+    // Search books here
+    console.log(query);
+  }
 
   render() {
     return (
-        <h1>BookFinder</h1>
+      <Wrapper>
+        <Page>
+          <Heading>Book Finder</Heading>
+          <SearchInput searchBooks={this.searchBooks} />
+        </Page>
+      </Wrapper>
     );
   }
 }
---
+++ src/js/components/SearchInput.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const InputWrapper = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 0;
+    max-width: 750px;
+    margin: 0 auto;
+
+    @media (min-width: 650px) {
+        flex-direction: row;
+    }
+`;
+
+const Input = styled.input`
+    box-sizing: border-box;
+    width: 100%;
+    height: 35px;
+    font-size: 18px;
+    padding: 5px 10px;
+
+    @media (min-width: 650px) {
+        width: 80%
+    }
+`;
+
+const Button = styled.button`
+    height: 35px;
+    width: 100%
+
+    @media (min-width: 650px) {
+        width: 120px;
+    }
+`;
+
+class SearchInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchTerm: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        if(e.target.value.length > 0) {
+            this.setState({
+                searchTerm: e.target.value
+            })
+        }
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const { searchTerm } = this.state;
+
+        if(searchTerm) {
+            this.props.searchBooks(searchTerm);
+        }
+    }
+    
+
+    render() {
+        return (
+            <InputWrapper onSubmit={this.onSubmit}>
+                <Input 
+                    placeholder="Search.."
+                    value={this.state.searchTerm}
+                    onChange={this.handleChange}
+                />
+                <Button>Search</Button>
+            </InputWrapper>        
+        )
+    }
+}
+
+export default SearchInput;
